a2699e68fec4571a21144a4ac9371e0a54ec2a88
Olivier <olivier@lesiteimmo.com>
2014-06-27 17:30:36 +0200

    updates deployment script according to new name

--- createArchive.sh
+++ createArchive.sh
@@ -5,8 +5,8 @@ VERSION=`git describe --tags --always`
 GIT_TOP_LEVEL=`git rev-parse --show-toplevel`
 OUTDIR=`readlink -e "$GIT_TOP_LEVEL/.."`
 
-OUTPATH="$OUTDIR/gedeon-sync-$VERSION.zip"
+OUTPATH="$OUTDIR/wp-realestate-sync-$VERSION.zip"
 
 echo "Creating archive $OUTPATH"
 
-git archive --format zip --prefix=gedeon-sync/ --output "$OUTPATH" master
+git archive --format zip --prefix=wp-realestate-sync/ --output "$OUTPATH" master
